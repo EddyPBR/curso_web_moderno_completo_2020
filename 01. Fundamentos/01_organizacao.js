4b4c6559836919772ea19a2d07240d7d122fae46
EddyPBR <edvaldojunior1310@gmail.com>
2020-05-19 09:18:00 -0300

    organizacao do javascript

---
+++ 01. Fundamentos/01_organizacao.js
@@ -0,0 +1,16 @@
+// O javascript é organizado em sentenças de código que podem ou não 
+// terminar em ponto e virgula
+
+console.log('hello javascript com ponto e vírgula');
+console.log('hello javascript sem ponto e vírgula')
+
+// e tambem podem ser organizados por sentenças de códigos que são 
+// delimitados por blocos de chaves, funções, objetos possuem 
+// blocos associados. 
+
+{
+  console.log('hello bloco mais externo');
+  {
+    console.log('hello bloco mais interno');
+  }
+}
