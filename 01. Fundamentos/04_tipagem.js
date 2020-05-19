ffbc0f96020928887bf8499a6f926427c79ce265
EddyPBR <edvaldojunior1310@gmail.com>
2020-05-19 09:40:52 -0300

    tipagens em javascript

---
+++ 01. Fundamentos/04_tipagem.js
@@ -0,0 +1,24 @@
+/**
+ * Linguagens de programação possutem tipos
+ * de variaveis/dados. São eles:
+ * inteiros, reais, chars, strings, booleanos, arrays, listas...
+ */
+
+/**
+ * Javascript é uma linguagem de tipagem fraca,
+ * isso significa que uma variável que foi data como um inteiro
+ * pode ser transformada em um tipo real futuramente.
+ */
+
+let qualquer = "Legal";
+console.log(qualquer);
+console.log(typeof qualquer);
+
+qualquer = 3.1516;
+console.log(qualquer);
+console.log(typeof qualquer);
+
+// Evite utilizar nomes genéricos e siglas
+let valor = '';
+let numero = 1;
+let pqp = false // Produto Químico Perigoso... kkk
