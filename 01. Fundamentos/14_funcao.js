f3b97030ad302969c46f5e6d8718649298e11e13
EddyPBR <edvaldojunior1310@gmail.com>
2020-05-19 14:52:14 -0300

    funcao em javascript

---
+++ 01. Fundamentos/14_funcao.js
@@ -0,0 +1,10 @@
+console.log(typeof Object);
+
+class Produto {};
+console.log(typeof Produto);
+
+/**
+ * Em resumo um objeto em javascript é
+ * uma função que é tratada de uma maneira
+ * um tanto quanto diferente.
+ */
