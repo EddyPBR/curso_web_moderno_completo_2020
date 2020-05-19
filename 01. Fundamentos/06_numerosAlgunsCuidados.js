fe79cc6a5634f015a996602ba340a5dac5c4a8eb
EddyPBR <edvaldojunior1310@gmail.com>
2020-05-19 10:05:52 -0300

    strings em javascript

--- 01. Fundamentos/06_numerosAlgunsCuidados.js
+++ 01. Fundamentos/06_numerosAlgunsCuidados.js
@@ -1,6 +1,8 @@
 console.log(7 / 0);
 console.log("10" / 2);
 console.log("10,2" / 2); // NaN = Not a Number
+console.log('3' + 2); 
+console.log('3' - 2); // neste caso a string é convertida para um valor número
 console.log("Show!" * 2);
 console.log(0.1 + 0.7);
 // console.log(10.toString()); // error
