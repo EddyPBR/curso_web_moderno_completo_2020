dcc419bc2ccc37cdde6862e6a35236e5adf0f82f
EddyPBR <edvaldojunior1310@gmail.com>
2020-05-20 17:15:45 -0300

    atividade 03

---
+++ 03. Funcoes/Lista_de_Exercicios/03_atividade.js
@@ -0,0 +1,10 @@
+/**
+ *  Crie uma função que recebe dois parâmetros, base e expoente, 
+ *  e retorne a base elevada ao expoente.
+ */
+
+const atividade03 = (base, expo) => Math.pow(base, expo);
+
+console.log(atividade03(2, 3));
+console.log(atividade03(2, 2));
+console.log(atividade03(2, 4));
