7d5628b7db26c59b020ac5040bbeffd536deeb02
EddyPBR <edvaldojunior1310@gmail.com>
2020-05-20 17:13:59 -0300

    atividade 02

---
+++ 03. Funcoes/Lista_de_Exercicios/02_atividade.js
@@ -0,0 +1,22 @@
+/**
+ * Os triângulos podem ser classificados em 3 tipos quanto ao tamanho de seus lados:
+ * Equilátero: Os três lados são iguais. 
+ * Isósceles: Dois lados iguais. 
+ * Escaleno: Todos os lados são diferentes.
+ * Crie uma função que recebe os comprimentos dos três lados de um triângulo e retorne sua 
+ * classificação quanto ao tamanho de seus lados. 
+ * (Neste exemplo deve-se abstrair as condições matemáticas de existência de um triângulo).
+ */
+
+const atividade02 = (a, b, c) => {
+  if(a === b && a === c) return("Equilátero");
+  if(a === b || a === c || b === c) return("Isósceles");
+  if(a !== b && a !== c && b !== c) return("Escaleno");
+}
+
+console.log(atividade02(1, 1, 1));
+console.log(atividade02(1, 1, 2));
+console.log(atividade02(1, 2, 2));
+console.log(atividade02(1, 2, 2));
+console.log(atividade02(1, 2, 3));
+console.log(atividade02(3, 2, 1));
