ba77c300ae2b5b40974ce69ead992dffcfe1edd7
EddyPBR <edvaldojunior1310@gmail.com>
2020-05-20 15:02:58 -0300

    Principios de closure

---
+++ 03. Funcoes/17_closure.js
@@ -0,0 +1,18 @@
+/**
+ * Closure
+ * É o escoco do criado quando uma função é declarada
+ * Esse escopo permite a função acessar e manipular variáveis externas à função.
+ */
+
+const x = 'Global';
+
+function fora() {
+  const x = 'Local'
+  function dentro() {
+    return x;
+  }
+  return dentro;
+}
+
+const minhaFuncao = fora();
+console.log(minhaFuncao());
