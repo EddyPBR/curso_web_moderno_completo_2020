dc210f1a634d6524f151f7e72bb162bddbf80733
EddyPBR <edvaldojunior1310@gmail.com>
2020-05-20 17:04:42 -0300

    adicionando diretorio de atividades e atividade01

---
+++ 03. Funcoes/Lista_de_Exercicios/01_atividade.js
@@ -0,0 +1,14 @@
+/**
+ * Crie uma função que dado dois valores (passados como parâmetros) mostre no console a soma, 
+ * subtração, multiplicação e divisão desses valores
+ */
+
+const atividade01 = (val1, val2) => {
+  console.log(`${val1} + ${val2} = ${val1 + val2}`);
+  console.log(`${val1} - ${val2} = ${val1 - val2}`);
+  console.log(`${val1} * ${val2} = ${val1 * val2}`);
+  console.log(`${val1} / ${val2} = ${val1 / val2}`);
+}
+
+atividade01(10, 2);
+atividade01(5, 2);
