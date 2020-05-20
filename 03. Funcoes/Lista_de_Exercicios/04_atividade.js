aaefa69426fb33977a1441cc472a56f6d6dbf5fd
EddyPBR <edvaldojunior1310@gmail.com>
2020-05-20 17:19:51 -0300

    atividade 04

---
+++ 03. Funcoes/Lista_de_Exercicios/04_atividade.js
@@ -0,0 +1,14 @@
+/**
+ * Crie uma função que irá receber dois valores, o dividendo e o divisor. 
+ * A função deverá imprimir o resultado e o resto da divisão destes dois valores
+ */
+
+const atividade04 = (dividendo, divisor) => {
+  console.log(`${dividendo} / ${divisor} = ${dividendo / divisor}`);
+  console.log(`${dividendo} % ${divisor} = ${dividendo % divisor}`);
+}
+
+atividade04(10, 2);
+atividade04(10, 5);
+atividade04(9, 3);
+atividade04(5, 2);
