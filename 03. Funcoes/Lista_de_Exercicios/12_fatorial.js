27a5b16b600105a08722868135aa191be01121c9
EddyPBR <edvaldojunior1310@gmail.com>
2020-05-21 14:39:29 -0300

    atividade 12

---
+++ 03. Funcoes/Lista_de_Exercicios/12_fatorial.js
@@ -0,0 +1,14 @@
+/**
+ * Faça um algoritmo que calcule o fatorial de um número;
+ */
+
+const calculaFatorial = (num) => {
+  return num == 0 ? 1 : num * calculaFatorial(num -1);
+}
+
+console.log(calculaFatorial(0));
+console.log(calculaFatorial(1));
+console.log(calculaFatorial(2));
+console.log(calculaFatorial(3));
+console.log(calculaFatorial(4));
+console.log(calculaFatorial(5));
