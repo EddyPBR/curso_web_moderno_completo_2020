942702e241ed6268f6b92c2556b5aa383ce77c49
EddyPBR <edvaldojunior1310@gmail.com>
2020-05-21 19:59:05 -0300

    atividade 26

---
+++ 03. Funcoes/Lista_de_Exercicios/26_pares.js
@@ -0,0 +1,9 @@
+/**
+ * Fazer um programa para encontrar todos os pares entre 1 e 100.
+ */
+
+let pares = [];
+for(let i = 1; i <= 100; i++) {
+  if (i % 2 === 0) pares.push(i);
+}
+console.log(pares);
