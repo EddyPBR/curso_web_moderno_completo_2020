f61bfa9a64227d87e8297c41f05203e843cb7033
EddyPBR <edvaldojunior1310@gmail.com>
2020-05-21 15:01:47 -0300

    atividade 15

---
+++ 03. Funcoes/Lista_de_Exercicios/15_comprarCarro.js
@@ -0,0 +1,29 @@
+/**
+ * Um homem decidiu ir à uma revenda comprar um carro. Ele deseja comprar um carro hatch, 
+ * e a revenda possui, além de carros hatch, sedans, motocicletas e caminhonetes. 
+ * Utilizando uma estrutura switch, caso o comprador queira o hatch, 
+ * retorne: “Compra efetuada com sucesso”.
+ *  Nas outras opções, 
+ * retorne: “Tem certeza que não prefere este modelo?”. 
+ * Caso seja especificado um modelo que não está disponível, 
+ * retorne no console: “Não trabalhamos com este tipo de automóvel aqui”.
+ */
+
+const escolherCarro = (nome) => {
+  switch(nome) {
+    case "hatch":
+      return "Compra efetuada com sucesso";
+    case "sedan":
+    case "motocicleta":
+    case "caminhonetes":
+      return "Tem certeza que não prefere este modelo?";
+    default:
+      return "Não trabalhamos com este tipo de automóvel aqui";
+  }
+}
+
+console.log(escolherCarro('hatch'));
+console.log(escolherCarro('motocicleta'));
+console.log(escolherCarro('sedan'));
+console.log(escolherCarro('caminhonete'));
+console.log(escolherCarro('jetski'));
