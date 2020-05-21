470a916573421fb3891f779f51259a217f778688
EddyPBR <edvaldojunior1310@gmail.com>
2020-05-21 14:05:41 -0300

    adicionando atividade 10

--- 03. Funcoes/Lista_de_Exercicios/10_atividade.js
+++ 03. Funcoes/Lista_de_Exercicios/10_atividade.js
@@ -2,3 +2,12 @@
  * Crie uma função que verifica se um número inteiro passado como parêmetro é divisível 
  * por 3 e retorne true ou false.
  */
+
+const atividade10 = (num) => {
+  if (num % 3 === 0) return true;
+  return false;
+}
+
+console.log(atividade10(3));
+console.log(atividade10(2));
+console.log(atividade10(150));
