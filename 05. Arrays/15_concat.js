5a4bfc858d0483254a701a03df0464559265d30f
EddyPBR <edvaldojunior1310@gmail.com>
2020-05-25 16:18:33 -0300

    Metodo concat

---
+++ 05. Arrays/15_concat.js
@@ -0,0 +1,6 @@
+const filhas = ['Irinéia', 'Hermanoteu'];
+const filhos = ['Irineu', 'Mikalahtéia'];
+const todos = filhas.concat(filhos);
+console.log(todos, filhas, filhos);
+
+console.log([].concat([1, 2], [3, 4], 5, [6, 7]));
