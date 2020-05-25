f8dcc795fe65182f507451ceb923c61563ea3538
EddyPBR <edvaldojunior1310@gmail.com>
2020-05-25 11:11:54 -0300

    JSON vs Objeto

---
+++ 04. Orientado a Objeto/15_JSONvsObjeto.js
@@ -0,0 +1,7 @@
+// JSON não carrega funcoes;
+const obj = { a: 1, b: 2, c: 3, soma() { return a + b + c }};
+console.log(JSON.stringify(obj));
+// console.log(JSON.parse('{ a: 1, b: 2, c: 3 }')); 
+// console.log(JSON.parse("{'a': 1,'b': 2,'c': 3}"));
+console.log(JSON.parse('{"a": 1, "b": 2, "c": 3}')); // Cada atributo é delimitado por aspas duplas
+console.log(JSON.parse('{"a": 1.7, "b": "string", "c": true, "d": {}, "e": [] }'));
