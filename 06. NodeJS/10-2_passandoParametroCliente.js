73afdcf3e87ec6022066fd46e9c0ccca64413d83
EddyPBR <Edvaldojunior1310@gmail.com>
2020-05-26 13:16:53 -0300

    Passando parametros entre modulos

---
+++ 06. NodeJS/10-2_passandoParametroCliente.js
@@ -0,0 +1,6 @@
+const saudacoes = require('./10-1_passandoParametros')('Ana', 'Lucas', 'João');
+console.log(saudacoes);
+
+// const saudacoes = require('./10-1_passandoParametros');
+// const listaSaudacoes = saudacoes('Ana', 'Lucas', 'João');
+// console.log(listaSaudacoes) // sim funciona!
