b2849e7519a72c4d354ed679ce159ff800130b69
EddyPBR <Edvaldojunior1310@gmail.com>
2020-05-26 13:11:41 -0300

    Sobre o this escopo global e module exports

---
+++ 06. NodeJS/09_this.js
@@ -0,0 +1,21 @@
+console.log(this === global);
+console.log(this === module);
+
+console.log(this === module.exports);
+console.log(this === exports);
+
+function logThis() {
+  console.log('Dentro de uma função...');
+  console.log(this === exports);
+  console.log(this === module.exports);
+  console.log(this === global);
+
+  this.perigo = '...';
+}
+
+logThis();
+
+/**
+ * Resumindo dentro de uma função o this aponta para global
+ * fora da função o this aponta para module.exports
+ */
